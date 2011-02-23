e3c476702f6740b31dd89be4d20fb953599bd19d
Tim Bielawa <timbielawa@gmail.com>
2011-02-23 01:48:13 -0500

    Add a README, hack up the Makefile a little big. Add Copyright header to urlang.erl

--- src/urlang.erl
+++ src/urlang.erl
@@ -1,10 +1,31 @@
+%%% Copyright (c) 2011 Tim 'Shaggy' Bielawa <timbielawa@gmail.com>
+%%% 
+%%% Permission is hereby granted, free of charge, to any person
+%%% obtaining a copy of this software and associated documentation
+%%% files (the "Software"), to deal in the Software without
+%%% restriction, including without limitation the rights to use,
+%%% copy, modify, merge, publish, distribute, sublicense, and/or sell
+%%% copies of the Software, and to permit persons to whom the
+%%% Software is furnished to do so, subject to the following
+%%% conditions:
+%%% 
+%%% The above copyright notice and this permission notice shall be
+%%% included in all copies or substantial portions of the Software.
+%%% 
+%%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+%%% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+%%% OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+%%% NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+%%% HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+%%% WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+%%% FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+%%% OTHER DEALINGS IN THE SOFTWARE.
+%%%
 %%% File    : urlang.erl
-%%% Author  : Tim Bielawa <timbielawa@gmail.com>
-%%% Description : URL Shortener
-%%% Created : 22 Feb 2011 by Tim Bielawa <timbielawa@gmail.com>
+%%% Description : URL Shortener in Erlang
 
 -module(urlang).
--export([init/0,url_store/0]).
+-export([init/0]).
 -include_lib("records.hrl").
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
